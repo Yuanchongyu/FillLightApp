efd78f4bec1ab05f43a13040210e290d602c0b4a
jxa98 <jxa98@sfu.ca>
2025-03-07 22:18:42 -0800

    Initial Commit

---
+++ Fill_Light/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fill_Light
+//
+//  Created by chongyuyuan on 2025-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fill_Light/Fill_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fill_LightApp.swift
+//  Fill_Light
+//
+//  Created by chongyuyuan on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct Fill_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
